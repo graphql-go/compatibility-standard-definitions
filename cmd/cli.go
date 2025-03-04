0ac61303bc00816a998570571a7b1543dc387d99
Chris Ramón <rchristian.ramon@gmail.com>
2025-03-04 16:42:06 -0500

    cmd: adds cmd pkg

---
+++ cmd/cli.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"graphql-go/compatibility-standard-definitions/bubbletea"
+)
+
+type CLI struct {
+}
+
+type model struct {
+}
+
+type RunResult struct {
+	Choice string
+}
+
+type RunParams struct {
+	Choices []string
+	Header  string
+}
+
+func (c *CLI) Run(p *RunParams) (*RunResult, error) {
+	bt := bubbletea.NewBubbleTea(&bubbletea.BubbleTeaParams{
+		Choices: p.Choices,
+		UI: bubbletea.BubbleTeaUIParams{
+			Header: p.Header,
+		},
+	})
+
+	btRunResult, err := bt.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	return &RunResult{
+		Choice: btRunResult.Choice,
+	}, nil
+}
