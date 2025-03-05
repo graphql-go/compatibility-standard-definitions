722412b8aa0e6fdf9d86a63670eaa17cbd460e2b
Chris Ramón <rchristian.ramon@gmail.com>
2025-03-05 16:21:54 -0500

    main: wires choices and header to cli component

--- main.go
+++ main.go
@@ -4,11 +4,25 @@ import (
 	"log"
 
 	"graphql-go/compatibility-standard-definitions/cmd"
+	"graphql-go/compatibility-standard-definitions/implementation"
 )
 
+var choices = []string{}
+
+func init() {
+	for _, i := range implementation.Implementations {
+		choices = append(choices, i.Repo.String(implementation.ImplementationPrefix))
+	}
+}
+
 func main() {
+	header := implementation.GraphqlSpecification.Repo.String(implementation.SpecificationPrefix)
+
 	cli := cmd.CLI{}
-	runResult, err := cli.Run(&cmd.RunParams{})
+	runResult, err := cli.Run(&cmd.RunParams{
+		Choices: choices,
+		Header:  header,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
