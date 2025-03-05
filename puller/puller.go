f748da585d8ae1163a8fe15aa3214516f20ccdb8
Chris Ramón <rchristian.ramon@gmail.com>
2025-03-05 10:57:03 -0500

    puller: adds puller component

---
+++ puller/puller.go
@@ -0,0 +1,48 @@
+package puller
+
+import (
+	"os"
+	"strings"
+
+	"github.com/go-git/go-git/v5"
+
+	"graphql-go/compatibility-unit-tests/types"
+)
+
+type Puller struct {
+}
+
+type PullerResult struct {
+}
+
+type PullerParams struct {
+	Specification     types.Specification
+	RefImplementation types.Implementation
+}
+
+func (p *Puller) Pull(params *PullerParams) (*PullerResult, error) {
+	repos := []types.Implementation{
+		params.Specification,
+		params.RefImplementation,
+	}
+
+	for _, r := range repos {
+		name := "./repos/" + r.Repo.Name
+		if _, err := os.Stat(name); os.IsNotExist(err) {
+			if err := os.Mkdir(name, os.ModePerm); err != nil {
+				return nil, err
+			}
+		}
+		if _, err := git.PlainClone(name, false, &git.CloneOptions{
+			URL:      r.Repo.URL,
+			Progress: os.Stdout,
+		}); err != nil {
+			if strings.Contains(err.Error(), "repository already exists") {
+				return nil, nil
+			}
+			return nil, err
+		}
+	}
+
+	return &PullerResult{}, nil
+}
