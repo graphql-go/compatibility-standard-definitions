ff0f22dc9e20ac5f2021dfad5d938a4ef94518ba
Chris Ramón <rchristian.ramon@gmail.com>
2025-03-06 15:55:23 -0500

    extractor: adds extractor implementation

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -1,5 +1,10 @@
 package extractor
 
+import (
+	"log"
+	"os"
+)
+
 type ExtractorParams struct{}
 type ExtractorResult struct{}
 
@@ -7,5 +12,25 @@ type Extractor struct {
 }
 
 func (e *Extractor) Extract(params *ExtractorParams) (*ExtractorResult, error) {
+	typeSystemStr, err := e.readTypeSystem()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println(typeSystemStr)
+
 	return &ExtractorResult{}, nil
+}
+
+func (e *Extractor) readTypeSystem() (string, error) {
+	f, err := os.ReadFile("./repos/graphql-specification/spec/Section 3 -- Type System.md")
+	if err != nil {
+		return "", err
+	}
+
+	log.Println(string(f))
+
+	// return strings.Split(string(f), "\n"), nil
+
+	return "", nil
 }
