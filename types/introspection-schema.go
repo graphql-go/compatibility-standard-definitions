ed54c9cf7f99a0d2828e9bb306e2be3131b4ee0f
Chris Ramón <rchristian.ramon@gmail.com>
2025-03-14 16:07:16 -0500

    types: syncs IntrospectionSchema Directives field

--- types/introspection-schema.go
+++ types/introspection-schema.go
@@ -1,10 +1,10 @@
 package types
 
 type IntrospectionSchema struct {
-	Description      string                  `json:"description"`
-	QueryType        IntrospectionObjectType `json:"queryType"`
-	MutationType     IntrospectionObjectType `json:"mutationType"`
-	SubscriptionType IntrospectionObjectType `json:"subscriptionType"`
-	Types            IntrospectionType       `json:"types"`
-	Directives       IntrospectionDirective  `json:"directives"`
+	Description      string                   `json:"description"`
+	QueryType        IntrospectionObjectType  `json:"queryType"`
+	MutationType     IntrospectionObjectType  `json:"mutationType"`
+	SubscriptionType IntrospectionObjectType  `json:"subscriptionType"`
+	Types            IntrospectionType        `json:"types"`
+	Directives       []IntrospectionDirective `json:"directives"`
 }
