d30a75d67c4ae7386c7cd4bb1493e8979dd91517
Chris Ramón <rchristian.ramon@gmail.com>
2025-03-18 16:41:43 -0500

    executor: wiring graphql-go/graphql executor resolution

--- executor/go.go
+++ executor/go.go
@@ -1,5 +1,28 @@
 package executor
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/graphql-go/graphql"
+)
+
+var RootQuery = graphql.ObjectConfig{
+	Name: "RootQuery",
+	Fields: graphql.Fields{
+		"echo": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "ok", nil
+			},
+		},
+	},
+}
+
+var SchemaConfig = graphql.SchemaConfig{
+	Query: graphql.NewObject(RootQuery),
+}
+
 // Go handles the go execution of a introspection query.
 type Go struct {
 }
@@ -16,5 +39,26 @@ type RunResult struct {
 
 // Run runs and returns a given introspection query.
 func (g *Go) Run(params *RunParams) (*RunResult, error) {
-	return &RunResult{}, nil
+	schema, err := graphql.NewSchema(SchemaConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	gqlParams := graphql.Params{
+		Schema:        schema,
+		RequestString: params.Query,
+	}
+	doResult := graphql.Do(gqlParams)
+	if doResult.Errors != nil {
+		return nil, fmt.Errorf("%+v", doResult.Errors)
+	}
+
+	result, err := json.Marshal(doResult)
+	if err != nil {
+		return nil, err
+	}
+
+	return &RunResult{
+		Result: string(result),
+	}, nil
 }
