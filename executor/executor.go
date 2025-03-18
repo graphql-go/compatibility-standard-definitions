dc1b23f50a6af123caecc664766ff46b9dcf5660
Chris Ramón <rchristian.ramon@gmail.com>
2025-03-18 11:52:42 -0500

    executor: adds initial structure of the executor component

---
+++ executor/executor.go
@@ -0,0 +1,18 @@
+package executor
+
+// Executor handles the resolution of a graphql introspection query.
+type Executor struct {
+}
+
+// ExecuteResult is the result of the execute method.
+type ExecuteResult struct {
+}
+
+// ExecuteParams is the params of the execute method.
+type ExecuteParams struct {
+}
+
+// Execute executes and returns the resolution of a graphql introspection query.
+func (e *Executor) Execute() (*ExecuteResult, error) {
+  return nil, nil
+}
